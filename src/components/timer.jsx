d81239c9b49a286b553419752c737bc821bd363d
Mohand Arezki <braniciarezki@gmail.com>
2022-12-02 06:02:34 +0100

    Added a timer

---
+++ src/components/timer.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { useRef } from "react";
+import { useState } from "react";
+
+const formatTime = (time) => {
+    let minutes = Math.floor(time / 60)
+    let seconds = Math.floor(time - minutes * 60)
+
+    if(minutes <= 9) minutes = '0' + minutes;
+    if(seconds <= 9) seconds =  '0' + seconds;
+
+    return minutes + ':' + seconds
+
+}
+
+export default function Timer({seconds}){
+    const [countDown, setCountDown] = useState(seconds);
+    const timerId = useRef()
+    
+        useEffect(() => {
+            timerId.current = setInterval(() =>{
+                setCountDown(prev => prev -1)
+            },1000)
+                return () => clearInterval(timerId.current)
+            }, [])
+            useEffect(() => {
+                if(countDown <= 0){
+                    clearInterval(timerId.current)
+                }
+            }, [countDown]
+            )
+
+    return(
+        <h2>Timer : {formatTime(countDown)}</h2>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Quiz from './components/quiz';
 
 ReactDOM.render(
   <React.StrictMode>
+    
+<Timer seconds={600}/>
     <Quiz />
   </React.StrictMode>,
   document.getElementById('root')
